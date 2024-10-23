2c9e7848631084a861985389185a1a4504111d9f
theoherv <theo.dancingdead@gmail.com>
2024-10-23 15:47:29 +0200

    EXOS

--- src/lab1.js
+++ src/lab1.js
@@ -1,4 +1,9 @@
-const http = require("http");
+import http from 'http';
+import { readFile, readFileSync } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 /**
  Todo Complete this app to serve the content inside `static` folder
@@ -10,8 +15,41 @@ const http = require("http");
  -- Hint, it possible to read a file using `readFileSync` of fs module and serve it to the client
  */
 
-const server = http.createServer((req, res) => {
-  //
-});
+const APP_JS_URL = "./src/static/js/app.js";
 
-server.listen(3000);
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    const filePath = join(__dirname, 'static', 'html', 'index.html');
+    readFile(filePath, (err, content) => {
+      if (err) {
+        res.writeHead(500);
+        res.end(`Server Error: ${err.code}`);
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(content, 'utf8');
+      }
+    });
+  }else if (req.url === '/about.html') {
+    const filePath = join(__dirname, 'static', 'html', 'about.html');
+    readFile(filePath, (err, content) => {
+      if (err) {
+        res.writeHead(500);
+        res.end(`Server Error: ${err.code}`);
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(content, 'utf8');
+      }
+    });
+  } else if (req.url === '/js/app.js') {
+    console.log("serving the app .js");
+    res.writeHead(200, { "Content-Type": "application/javascript" });
+    const appJs = readFileSync(APP_JS_URL);
+    res.end(appJs);
+  } else {
+    res.writeHead(404);
+    res.end('Page not found');
+  }
+});
+ 
+ const PORT = process.env.PORT || 3000;
+ server.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
